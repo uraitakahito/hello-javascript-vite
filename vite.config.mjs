1d7c1ed4f6249f17597f806891ad0f5f878311ec
Takahito Urai <uraitakahito@gmail.com>
2024-12-10 05:49:18 +0900

    Add Vite configuration file for project setup

---
+++ vite.config.mjs
@@ -0,0 +1,11 @@
+// Vitest supports the same extensions for your configuration file as Vite does:
+// .js, .mjs, .cjs, .ts, .cts, .mts.
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // test: {
+  //   globals: true,
+  // },
+});
