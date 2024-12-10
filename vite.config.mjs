1187766b763c1bd1c1c49e3792dd86b5aaadfd80
Takahito Urai <uraitakahito@gmail.com>
2024-12-10 10:10:02 +0900

    Update Vite configuration to specify Rollup input for the build process

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,16 @@
 // Vitest supports the same extensions for your configuration file as Vite does:
 // .js, .mjs, .cjs, .ts, .cts, .mts.
 
+import { resolve } from 'path';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // test: {
-  //   globals: true,
-  // },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
 });
